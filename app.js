607433807d7deba2fcbca9dcd1cfdaa760453b34
wnicolass <nicolaswgl99@gmail.com>
2023-05-02 23:33:44 +0100

    feat: import and use the ad routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import express from 'express';
 import homeRoutes from './src/routes/home.routes';
 import tokenRoutes from './src/routes/token.routes';
 import userRoutes from './src/routes/user.routes';
+import adRoutes from './src/routes/ad.routes';
 import errorHandler from './src/middlewares/errorHandler';
 
 export default (function app() {
@@ -21,6 +22,7 @@ export default (function app() {
       myApp.use('/', homeRoutes);
       myApp.use('/token', tokenRoutes);
       myApp.use('/users', userRoutes);
+      myApp.use('/ads', adRoutes);
       myApp.use(errorHandler);
     }()),
   };
