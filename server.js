f0983ce24c6540d98e0e56bcd86d433497087a18
wnicolass <nicolaswgl99@gmail.com>
2023-05-04 17:06:15 +0100

    feat: export app to be used on tests

--- server.js
+++ server.js
@@ -3,3 +3,5 @@ import { default as server } from './app';
 const { app } = server;
 const port = 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+export default app;
