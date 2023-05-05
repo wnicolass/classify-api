72d4686077ddef129e3c4177123a1fa0fdcda8d4
wnicolass <nicolaswgl99@gmail.com>
2023-05-05 16:10:53 +0100

    feat: ignore some files

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ export default {
   transform: {},
   setupFiles: ['./tests/setup.js'],
   verbose: true,
+  modulePathIgnorePatterns: ['./tests/user.test.js', './tests/subcategory.test.js', './tests/ad.test.js', './tests/category.test.js'],
 };
